fdca2cddc409635d52a5be893f4ae581a6404cc3
Jeru02 <boatengj006472@gmail.com>
2025-04-28 11:26:26 +0100

    refactor code based on comments

--- api.js
+++ api.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const {getApi} = require('./app/controller/documentation.controller')
-app.use(express.json());
 
 
 app.get("/api", getApi)
