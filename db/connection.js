d3f02e2371cd9ac3b087fdf15f2ab479bf8b0390
Jeru02 <boatengj006472@gmail.com>
2025-05-01 16:05:30 +0100

    refactor connection file

--- db/connection.js
+++ db/connection.js
@@ -2,25 +2,18 @@ const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || "development";
 
+//loads the environment variables in the .env file of production/ test / developmemnt using dotenv config
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
 
-//loads the environment vaibles in the .env file of production/ test / developmemnt
 const config = {};
-
+//if we are tyring to connect to our production db we need to send the url with its password in the .env file
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 }
 
-module.exports = new Pool(config);
-
-//error handling
-if (!process.env.PGDATABASE) {
-  throw new Error("No PGDATABASE configured");
-} else {
-  console.log(`Connected to ${process.env.PGDATABASE}`);
-}
-
+//error if no database is connected
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
+module.exports = new Pool(config);
